381c4e1d85b5fcc1ca69e39a5daff7afa85ddb15
Sverine <severine.sylvan@gmail.com>
2021-10-04 15:00:56 +0200

    configure bootstrap

--- assets/app.js
+++ assets/app.js
@@ -7,10 +7,14 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/global.scss';
-import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
+
+//import 'bootstrap';
+//import bsCustomFileInput from 'bs-custom-file-input';
+
+// You can specify which plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
-bsCustomFileInput.init();
+//bsCustomFileInput.init();
 
