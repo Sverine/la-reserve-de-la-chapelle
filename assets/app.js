7c01999686ac4f74c00fa05cda0dd71e14a8b26f
Sverine <severine.sylvan@gmail.com>
2021-10-04 23:22:47 +0200

    add homepage

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/global.scss';
 import 'bootstrap';
-import bsCustomFileInput from 'bs-custom-file-input';
+//import bsCustomFileInput from 'bs-custom-file-input';
 
 // start the Stimulus application
 import './bootstrap';
-bsCustomFileInput.init();
+//bsCustomFileInput.init();
 
